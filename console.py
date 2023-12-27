a826c20624305ba82e0800732cbca01a93259fe8
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2023-12-27 16:34:37 +0100

    task 2

--- console.py
+++ console.py
@@ -115,7 +115,6 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        """ Create an object of any class"""
         try:
             if not args:
                 raise SyntaxError()
