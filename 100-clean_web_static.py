f2fe867a49dab3c699cb5a591aec691c307aa62b
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-01-11 22:59:51 +0100

    update task 4

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -6,6 +6,53 @@ from fabric.api import *
 env.hosts = ["100.24.236.248", "100.26.151.181"]
 
 
+@task
+def do_pack():
+    """Generate an tgz archive from web_static folder"""
+    try:
+        time_str = time.strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/"
+              .format(time_str))
+        return ("versions/web_static_{}.tgz".format(time_str))
+    except Except:
+        return None
+
+
+@task
+def do_deploy(archive_path):
+    """Distribute an archive to web servers"""
+    if (os.path.isfile(archive_path) is False):
+        return False
+
+    try:
+        file = archive_path.split("/")[-1]
+        folder = ("/data/web_static/releases/" + file.split(".")[0])
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(folder))
+        run("tar -xzf /tmp/{} -C {}".format(file, folder))
+        run("rm /tmp/{}".format(file))
+        run("mv {}/web_static/* {}/".format(folder, folder))
+        run("rm -rf {}/web_static".format(folder))
+        run('rm -rf /data/web_static/current')
+        run("ln -s {} /data/web_static/current".format(folder))
+        print("Deployment done")
+        return True
+    except Except:
+        return False
+
+
+@task
+def deploy():
+    """Create and distributes an archive to web servers"""
+    try:
+        path = do_pack()
+        return do_deploy(path)
+    except Except:
+        return False
+
+
+@task
 def do_clean(number=0):
     """ CLEANS """
 
