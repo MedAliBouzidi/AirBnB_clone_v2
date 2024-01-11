c4140e875d2c47c102fa6970ed9ebd8fbce51b71
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-01-11 15:59:40 +0100

    update task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,10 +7,10 @@ import time
 def do_pack():
     """Generate an tgz archive from web_static folder"""
     try:
+        time_str = time.strftime("%Y%m%d%H%M%S")
         local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(time.strftime("%Y%m%d%H%M%S")))
-        return ("versions/web_static_{}.tgz".format(time.
-                                                    strftime("%Y%m%d%H%M%S")))
+        local("tar -cvzf versions/web_static_{}.tgz web_static/"
+              .format(time_str))
+        return ("versions/web_static_{}.tgz".format(time_str))
     except Except:
         return None
