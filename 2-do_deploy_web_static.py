277543a66f96dee74c4dbd7bc233e78ee1a9c9a5
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-01-11 16:42:19 +0100

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["44.210.150.159", "35.173.47.15"]
+env.hosts = ["100.24.236.248", "100.26.151.181"]
 env.user = "ubuntu"
 
 
