f3d957dd6c4afd0a11a5c3d39bc54dd57c5e1c7a
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-01-11 17:04:37 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Setup a web servers for the deployment of web_static.
-apt update -y
-apt install -y nginx
+sudo apt update -y
+sudo apt install -y nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
 printf %s "<!DOCTYPE html>
 <html>
@@ -13,11 +13,11 @@ printf %s "<!DOCTYPE html>
   <body>
     <p>Nginx server test</p>
   </body>
-</html>" | tee /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data
 
 sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 
