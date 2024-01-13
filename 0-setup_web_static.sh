4872082761a67a5335b8cdd03adaf371631e4a19
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-01-13 01:01:28 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -24,6 +24,6 @@ then
   sudo sed -i '$i\\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-enabled/default
 fi
 
-sudo ln -sf /etc/nginx/sites-enabled /etc/nginx/sites-available
+sudo ln -sf /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
 
 sudo service nginx restart
