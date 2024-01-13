ef256e04c3bdbb9b49eaaa0bc951915b899d21a5
BOUZIDI Mohamed Ali <bouzidi.medali20@gmail.com>
2024-01-13 01:09:43 +0100

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -21,9 +21,7 @@ sudo chown -R ubuntu:ubuntu /data
 
 if ! grep -q "location /hbnb_static" /etc/nginx/sites-enabled/default;
 then
-  sudo sed -i '$i\\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-enabled/default
+  sudo sed -i '$i\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-enabled/default
 fi
-
-sudo ln -sf /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
 
 sudo service nginx restart
